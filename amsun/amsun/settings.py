818e1ee8b77d699c2d67850bae32310893a75295
Joel Masiaga <masiagajoel001@gmail.com>
2026-04-07 01:47:27 +0300

    Add Whitenoise for static file storage and update requirements.txt

--- amsun/amsun/settings.py
+++ amsun/amsun/settings.py
@@ -123,5 +123,6 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = 'users.User'
