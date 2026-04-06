2400851b2726b2586a382a2c1258d40bf642c07f
Joel Masiaga <masiagajoel001@gmail.com>
2026-04-07 01:27:33 +0300

    Update ALLOWED_HOSTS to allow all hosts during development

--- amsun/amsun/settings.py
+++ amsun/amsun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4h(uvxuskn08g$=n__=6hdwrhot%u#nijs@$)uy@zfo7rb!9%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
